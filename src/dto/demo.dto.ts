ea38b65ba8c837e7d30625e65cbfb10a0690635f
Gavin <gavinwjw@163.com>
2024-07-23 11:12:08 +0800

    update code

--- src/dto/demo.dto.ts
+++ src/dto/demo.dto.ts
@@ -10,6 +10,8 @@ export class CreateItemDTO {
   description: string;
 }
 
+// 当我传 [] 时，CreateDTO 的参数并没有追加到 UpdateDTO 中（并可选），
+// 只有我写了一个排除的属性，才会把剩余的追加到 UpdateDTO 并且可选
 export class UpdateItemDTO extends OptionalDto(CreateItemDTO, []) {
   id: number;
 }
